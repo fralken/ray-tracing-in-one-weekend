c64db84e4071b3df97ed9b8d578014eebe9304e4
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2019-04-03 16:47:49 +0200

    chapter 5.1

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,23 @@ mod ray;
 use nalgebra::Vector3;
 use crate::ray::Ray;
 
-fn hit_sphere(center: &Vector3<f32>, radius: f32, ray: &Ray) -> bool {
+fn hit_sphere(center: &Vector3<f32>, radius: f32, ray: &Ray) -> Option<f32> {
     let oc = ray.origin() - center;
     let a = ray.direction().dot(&ray.direction());
     let b = 2.0 * oc.dot(&ray.direction());
     let c = oc.dot(&oc) - radius.powi(2);
     let discriminant = b.powi(2) - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
+    }
 }
 
 fn color(ray: &Ray) -> Vector3<f32> {
-    if hit_sphere(&Vector3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        Vector3::new(1.0, 0.0, 0.0)
+    if let Some(t) = hit_sphere(&Vector3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        let n = (ray.point_at_parameter(t) - Vector3::new(0.0, 0.0, -1.0)).normalize();
+        0.5 * n.add_scalar(1.0)
     } else {
         let unit_direction = ray.direction().normalize();
         let t = 0.5 * (unit_direction[1] + 1.0);
