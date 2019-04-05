32019791127f22e41f1db9fc69b274b246bad0a5
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2019-04-05 14:17:56 +0200

    chapter 11

--- src/camera.rs
+++ src/camera.rs
@@ -1,31 +1,50 @@
 use std::f32;
 use nalgebra::Vector3;
+use rand::Rng;
 use crate::ray::Ray;
+
+fn random_in_unit_disk() -> Vector3<f32> {
+    let mut rng = rand::thread_rng();
+    let unit = Vector3::new(1.0, 1.0, 0.0);
+    loop {
+        let p = 2.0 * Vector3::new(rng.gen::<f32>(), rng.gen::<f32>(), 0.0) - unit;
+        if p.dot(&p) < 1.0 {
+            return p
+        }
+    }
+}
 
 pub struct Camera {
     origin: Vector3<f32>,
     lower_left_corner: Vector3<f32>,
     horizontal: Vector3<f32>,
-    vertical: Vector3<f32>
+    vertical: Vector3<f32>,
+    u: Vector3<f32>,
+    v: Vector3<f32>,
+    lens_radius: f32
 }
 
 impl Camera {
-    pub fn new(look_from: Vector3<f32>, look_at: Vector3<f32>, view_up: Vector3<f32>, vertical_fov: f32, aspect: f32) -> Self {
+    pub fn new(look_from: Vector3<f32>, look_at: Vector3<f32>, view_up: Vector3<f32>, vertical_fov: f32, aspect: f32, aperture: f32, focus_dist: f32) -> Self {
         let theta = vertical_fov * f32::consts::PI / 180.0;
-        let half_height = f32::tan(theta / 2.0);
+        let half_height = focus_dist * f32::tan(theta / 2.0);
         let half_width = aspect * half_height;
         let w = (look_from - look_at).normalize();
         let u = view_up.cross(&w).normalize();
         let v = w.cross(&u);
         Camera {
             origin: look_from,
-            lower_left_corner: look_from - half_width * u - half_height * v - w,
+            lower_left_corner: look_from - half_width * u - half_height * v - focus_dist * w,
             horizontal: 2.0 * half_width * u,
-            vertical: 2.0 * half_height * v
+            vertical: 2.0 * half_height * v,
+            u, v,
+            lens_radius: aperture / 2.0
         }
     }
 
     pub fn get_ray(&self, s: f32, t: f32) -> Ray {
-        Ray::new(self.origin, self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin)
+        let rd = self.lens_radius * random_in_unit_disk();
+        let offset = self.u * rd.x + self.v * rd.y;
+        Ray::new(self.origin + offset, self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -41,11 +41,11 @@ fn main() {
         Box::new(Sphere::new(Vector3::new(-1.0, 0.0, -1.0), 0.5, Dielectric::new(1.5))),
         Box::new(Sphere::new(Vector3::new(-1.0, 0.0, -1.0), -0.45, Dielectric::new(1.5)))
     ]);
-    let cam = Camera::new(
-        Vector3::new(-2.0, 2.0, 1.0),
-        Vector3::new(0.0, 0.0, -1.0),
-        Vector3::new(0.0, 1.0, 0.0),
-        20.0, nx as f32 / ny as f32);
+    let look_from = Vector3::new(3.0, 3.0, 2.0);
+    let look_at = Vector3::new(0.0, 0.0, -1.0);
+    let focus_dist = (look_from - look_at).magnitude();
+    let aperture = 2.0;
+    let cam = Camera::new(look_from, look_at, Vector3::new(0.0, 1.0, 0.0), 20.0, nx as f32 / ny as f32, aperture, focus_dist);
     for j in (0..ny).rev() {
         for i in 0..nx {
             let mut col = Vector3::new(0.0, 0.0, 0.0);
