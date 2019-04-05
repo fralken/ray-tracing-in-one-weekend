700914efd9a7428c489f32a402bff13607d13973
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2019-04-05 10:20:14 +0200

    chapter 10.1

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,4 @@
+use std::f32;
 use nalgebra::Vector3;
 use crate::ray::Ray;
 
@@ -9,16 +10,19 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new() -> Self {
+    pub fn new(vertical_fov: f32, aspect: f32) -> Self {
+        let theta = vertical_fov * f32::consts::PI / 180.0;
+        let half_height = f32::tan(theta / 2.0);
+        let half_width = aspect * half_height;
         Camera {
             origin: Vector3::new(0.0, 0.0, 0.0),
-            lower_left_corner: Vector3::new(-2.0, -1.0, -1.0),
-            horizontal: Vector3::new(4.0, 0.0, 0.0),
-            vertical: Vector3::new(0.0, 2.0, 0.0)
+            lower_left_corner: Vector3::new(-half_width, -half_height, -1.0),
+            horizontal: Vector3::new(2.0 * half_width, 0.0, 0.0),
+            vertical: Vector3::new(0.0, 2.0 * half_height, 0.0)
         }
     }
 
     pub fn get_ray(&self, u: f32, v: f32) -> Ray {
-        Ray::new(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical)
+        Ray::new(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin)
     }
 }
